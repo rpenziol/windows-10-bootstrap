9a5ea38d311ceec80977d03d1510a95c2b6ca121
Robbie Penziol <rpenziol@users.noreply.github.com>
2022-02-27 23:13:48 -0800

    Make Choco install command easier to maintain. Make list of packages comprehensive.

--- install_tools.cmd
+++ install_tools.cmd
@@ -5,23 +5,59 @@ if NOT %errorLevel% == 0 (
 	exit
 )
 
+@REM Install Chocolatey package manager
 PowerShell -Command Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install 7zip.install -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install agentransack -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install chocolateypackageupdater -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install git -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install google-backup-and-sync -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install googlechrome -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install python3 -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install retroarch -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install revo-uninstaller -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install steam-client -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install synctrayzor -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install vlc -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install vscode -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install windirstat -y
-PowerShell -Command C:\ProgramData\chocolatey\bin\choco.exe install winscp -y
 
+@REM Install packages using Chocolatey
+C:\ProgramData\chocolatey\bin\choco.exe install -y ^
+agentransack ^
+audacity ^
+calibre ^
+coretemp ^
+crystaldiskinfo ^
+crystaldiskmark ^
+discord ^
+docker-desktop ^
+eartrumpet ^
+ffmpeg ^
+Firefox ^
+foobar2000 ^
+foxitreader ^
+git ^
+googleearthpro ^
+handbrake ^
+hwinfo ^
+InkScape ^
+iTunes ^
+libreoffice-fresh ^
+lossless-audio-checker ^
+lossless-cut ^
+meld ^
+microsoft-windows-terminal ^
+mkvtoolnix ^
+nomacs ^
+obs-studio ^
+postman ^
+python3 ^
+renamer ^
+retroarch ^
+rufus ^
+sqlitebrowser ^
+steam ^
+synctrayzor ^
+sysinternals ^
+tagscanner ^
+thunderbird ^
+veracrypt ^
+vlc ^
+vscode ^
+win32diskimager ^
+windirstat ^
+winscp ^
+wsl-ubuntu-2004 ^
+wsl2
+
+@REM Install OpenSSH Server, enable and start OpenSSH Server service
 PowerShell -Command "Add-WindowsCapability -Online -Name (Get-WindowsCapability -Online | ? Name -like 'OpenSSH.Server*').Name"
 sc start sshd
 sc config sshd start=auto
